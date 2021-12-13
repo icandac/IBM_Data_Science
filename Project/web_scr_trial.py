f7ed7c35697b9e8052ec6556c20768a0cbeafab3
I. Ceyhun Andaç <icandac@users.noreply.github.com>
2021-12-13 18:00:48 +0100

    A small trial for web scraping

---
+++ Project/web_scr_trial.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/netflix_data_webpage.html"
+
+data  = requests.get(url).text
+soup = BeautifulSoup(data, 'html5lib')
+
+netflix_data = pd.DataFrame(columns=["Date", "Open", "High", "Low", "Close", "Volume"])
+
+# First we isolate the body of the table which contains all the information
+# Then we loop through each row and find all the column values for each row
+for row in soup.find("tbody").find_all('tr'):
+    col = row.find_all("td")
+    date = col[0].text
+    Open = col[1].text
+    high = col[2].text
+    low = col[3].text
+    close = col[4].text
+    adj_close = col[5].text
+    volume = col[6].text
+    
+    # Finally we append the data of each row to the table
+    netflix_data = netflix_data.append({"Date":date, "Open":Open, "High":high, "Low":low, "Close":close, "Adj Close":adj_close, "Volume":volume}, ignore_index=True)    
+
+# netflix_data.head()
+
+read_html_pandas_data = pd.read_html(url) # OR read_html_pandas_data = pd.read_html(str(soup))
+
+netflix_dataframe = read_html_pandas_data[0]
+
+# netflix_dataframe.head()
+
+##########################################################
+
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/amazon_data_webpage.html"
+
+html_data  = requests.get(url).text
+
+html_soup = BeautifulSoup(html_data, 'html5lib')
+
+# html_soup.title
+
+amazon_data = pd.DataFrame(columns=["Date", "Open", "High", "Low", "Close", "Volume"])
+
+for row in soup.find("tbody").find_all("tr"):
+    col = row.find_all("td")
+    date = col[0].text
+    Open = col[1].text
+    high = col[2].text
+    low = col[3].text
+    close = col[4].text
+    adj_close = col[5].text
+    volume = col[6].text
+    
+    amazon_data = amazon_data.append({"Date":date, "Open":Open, "High":high, "Low":low, "Close":close, "Adj Close":adj_close, "Volume":volume}, ignore_index=True)
+
+# amazon_data.head()
+
+# amazon_data.Open.iloc[-1]
